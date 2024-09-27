7c75fc608ecb993ec37971d2e65bf20865ae0592
Brian Laub <blaub@palantir.com>
2024-09-27 11:00:57 -0400

    remove unused JdkDistributionName.LOOM_EA

--- gradle-jdks-distributions/src/main/java/com/palantir/gradle/jdks/JdkDistributionName.java
+++ gradle-jdks-distributions/src/main/java/com/palantir/gradle/jdks/JdkDistributionName.java
@@ -25,8 +25,7 @@ public enum JdkDistributionName {
     AZUL_ZULU,
     AMAZON_CORRETTO,
     GRAALVM_CE,
-    JAVA_EARLY_ACCESS,
-    LOOM_EA;
+    JAVA_EARLY_ACCESS;
 
     JdkDistributionName() {}
 
