e269cf67afe0fff5b61da8edbfeaa481d8d9db83
Claudia Rogoz <crogoz@palantir.com>
2024-10-24 16:26:02 +0200

    checl

--- gradle-jdks-setup/src/integrationTest/resources/testing-script.sh
+++ gradle-jdks-setup/src/integrationTest/resources/testing-script.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+if ! "$GRADLE_DAEMON_JDK/bin/java" -version 2>&1 | grep -q "11.0.21.9.1"; then
+  echo "Invalid JAVA_HOME: $GRADLE_DAEMON_JDK" >&2
+fi
 /root/.gradle/gradle-jdks/amazon-corretto-11.0.21.9.1/bin/java -version
 if [ -f /palantir.crt ];then
   echo "Palantir cert:" $(/root/.gradle/gradle-jdks/amazon-corretto-11.0.21.9.1/bin/keytool -keystore /root/.gradle/gradle-jdks/amazon-corretto-11.0.21.9.1/lib/security/cacerts -list -alias gradlejdks_palantir3rd-generationrootca -storepass changeit)
