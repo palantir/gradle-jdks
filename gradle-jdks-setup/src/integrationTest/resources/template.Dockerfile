2447cc12dc9eb24d3f26273c984f05800c8fd995
Claudia Rogoz <crogoz@palantir.com>
2024-10-25 18:39:57 +0200

    Set `JAVA_HOME` in gradle-jdks-setup script (#447)
    gradle-jdks-setup.sh script sets `JAVA_HOME` to gradle JDK

--- gradle-jdks-setup/src/integrationTest/resources/template.Dockerfile
+++ gradle-jdks-setup/src/integrationTest/resources/template.Dockerfile
@@ -6,9 +6,7 @@ ARG INSTALL_CURL=false
 # Update package lists and conditionally install curl
 RUN if [ "$INSTALL_CURL" = "true" ] ; then \
         apt-get update && \
-        apt-get install -y curl ; \
+        apt-get install -y curl; \
     fi
 COPY . /
-RUN mkdir -p /etc/ssl/certs && cat /example.com.crt >> /etc/ssl/certs/ca-bundle.crt
-RUN if [ -f /palantir.crt ]; then cat /palantir.crt >> /etc/ssl/certs/ca-bundle.crt; else echo "File does not exist."; fi
 RUN $SCRIPT_SHELL /gradle/gradle-jdks-setup.sh
--- gradle-jdks-setup/src/integrationTest/resources/testing-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-/root/.gradle/gradle-jdks/amazon-corretto-11.0.21.9.1/bin/java -version
-if [ -f /palantir.crt ];then
-  echo "Palantir cert:" $(/root/.gradle/gradle-jdks/amazon-corretto-11.0.21.9.1/bin/keytool -keystore /root/.gradle/gradle-jdks/amazon-corretto-11.0.21.9.1/lib/security/cacerts -list -alias gradlejdks_palantir3rd-generationrootca -storepass changeit)
-fi
-echo "Example.com cert:" $(/root/.gradle/gradle-jdks/amazon-corretto-11.0.21.9.1/bin/keytool -keystore /root/.gradle/gradle-jdks/amazon-corretto-11.0.21.9.1/lib/security/cacerts -list -alias gradleJdks_example.com -storepass changeit)
