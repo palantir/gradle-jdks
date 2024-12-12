8de8eecc5e086527d6f18171509b932fa96bb6d1
Claudia Rogoz <crogoz@palantir.com>
2024-12-12 14:42:34 +0100

    fixes

--- gradle-jdks-setup/src/main/resources/gradle-jdks-functions.sh
+++ gradle-jdks-setup/src/main/resources/gradle-jdks-functions.sh
@@ -145,7 +145,7 @@ install_and_setup_jdks() {
           tar -xzf "$distribution_name"
           ;;
         *)
-          curl -k -C - "$distribution_url" | tar -xzf -
+          curl -C - "$distribution_url" | tar -xzf -
           ;;
       esac
     elif command -v wget > /dev/null 2>&1; then
