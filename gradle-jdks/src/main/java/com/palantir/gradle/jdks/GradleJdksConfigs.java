4f6e42229f027f3a6adcdb3bd2d125a0caeaa849
Finlay Williams <finlayw@palantir.com>
2025-02-10 15:26:58 +0000

    update error message

--- gradle-jdks/src/main/java/com/palantir/gradle/jdks/GradleJdksConfigs.java
+++ gradle-jdks/src/main/java/com/palantir/gradle/jdks/GradleJdksConfigs.java
@@ -96,7 +96,9 @@ public abstract class GradleJdksConfigs extends DefaultTask {
             throw new RuntimeException(
                     "No JDKs were configured for the gradle setup. Please run `./gradlew setupJdks` to generate the"
                             + " JDKs and ensure that you have configured JDKs properly for gradle-jdks as per"
-                            + " the readme: https://github.com/palantir/gradle-jdks#usage");
+                            + " the readme: https://github.com/palantir/gradle-jdks#usage"
+                            + " if you see this error after removing all `gradle-sls-docker` plugins"
+                            + " see: https://pl.ntr/2v1");
         }
 
         String gradleJdkDaemonVersion = getDaemonJavaVersion().get().toString();
